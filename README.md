b45d2da8a0e17c7645121e0d3560a3a563150021
dan <github@danielsteinke.com>
2019-08-11 20:50:55 -0500

    Added countdown functionality
    Added functionality so that this widget can easily be used as both a stopwatch-style timer or a countdown clock.

--- README.md
+++ README.md
@@ -12,14 +12,13 @@ Example usage:
  timer_widget = tktimerwidget.TkTimer(main_window, buttons=True)
 
 ##Class provides the following methods:
- timer_widget.start_Timer
  timer_widget.start_timer()
  timer_widget.stop_timer()
  timer_widget.reset_timer()
  ##'update_widget' updates the StringVar used by the widget, normally this
  ## gets called automatically.
  timer_widget.update_widget()
- timer_widget.add_seconds()
- timer_widget.subtract_seconds()
- timer_widget.set_elapsed()
+ timer_widget.add_seconds(10)
+ timer_widget.subtract_seconds(5)
+ timer_widget.set_elapsed(99)
  ```
--- tktimerwidget.py
+++ tktimerwidget.py
@@ -20,44 +20,58 @@ class TkTimer(object):
         self._timer_label.pack()
 
         if buttons:
-            play_btn = tk.Button(self.widget, text="Start", command=self.start_timer)
-            play_btn.pack()
-            stop_btn = tk.Button(self.widget, text="Stop", command=self.stop_timer)
+            start_btn = tk.Button(self.widget, text="Start Timer", command=self.start_timer)
+            start_btn.pack()
+            stop_btn = tk.Button(self.widget, text="Stop", command=self.stop)
             stop_btn.pack()
-            reset_btn = tk.Button(self.widget, text="Reset", command=self.reset_timer)
+            reset_btn = tk.Button(self.widget, text="Reset", command=self.reset)
             reset_btn.pack()
 
-        self.widget.pack()
+            countdown_entry = tk.Entry(self.widget, width=3)
+            countdown_entry.pack()
+            countdown_seconds = lambda: self.start_countdown(int(countdown_entry.get()))
+            countdown_start_btn = tk.Button(self.widget, text="Start Countdown", command=countdown_seconds)
+            countdown_start_btn.pack()
 
+
+        self.widget.pack()
+        self.reset()
     def start_timer(self):
         if not self.timer.is_running():
             self.timer.start_timer()
             self.run_loop()
 
-    def stop_timer(self):
-        self.timer.stop_timer()
+    def start_countdown(self, countdown_seconds):
+        if not self.timer.is_running():
+            self.timer.start_countdown(countdown_seconds)
+            self.run_loop()
 
-    def reset_timer(self):
-        self.timer.reset_timer()
+    def stop(self):
+        self.timer.stop()
+
+    def reset(self):
+        self.timer.reset()
         self.update_widget(True)
 
     def update_widget(self, force_update = False):
         if self.timer.is_running() or force_update:
             __, elapsed_str = self.timer.get_elapsed()
             self._timer_stringvar.set(elapsed_str)
+
     def set_elapsed(self, time_in_seconds):
         self.timer.set_elapsed(time_in_seconds)
-        self.update_widget(True)
+        self.update_widget(force_update=True)
 
     def add_seconds(self, seconds_to_add):
         self.timer.add_seconds(seconds_to_add)
-        self.update_widget(True)
+        self.update_widget(force_update=True)
 
     def subtract_seconds(self, seconds_to_subtract):
         self.timer.subtract_seconds(seconds_to_subtract)
-        self.update_widget(True)
+        self.update_widget(force_update=True)
 
     def run_loop(self):
         if self.timer.is_running():
             self.update_widget()
             self.root.after(1, self.run_loop)
+        self.update_widget(True)
